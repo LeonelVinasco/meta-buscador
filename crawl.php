9a7fb6c4b28244d540145e7c77f1b600e769fb10
leo <leonelvinasco@gmail.com>
2019-06-09 23:31:45 -0500

    Busca todos los enlaces que hay en el sitio web especificado, también realiza el filtrado y la organización de los enlaces relativos y otros

--- crawl.php
+++ crawl.php
@@ -3,6 +3,29 @@
 //Se importa la clase DomDocumentParser
 include("classes/DomDocumentParser.php");
 
+function createLink($src, $url) {
+    $scheme= parse_url($url)["scheme"]; //http or HttpQueryString
+    $host = parse_url($url)["host"]; // www.reecekenney.com
+
+    if (substr($src,0,2) == "//"){
+      $src = $scheme . ":" . $src;
+    }
+    else if(substr($src,0,1) == "/"){
+      $src= $scheme . "://" . $host . $src;
+    }
+    else if (substr($src,0,2) == "./"){
+      $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src,1); //toma todo el texto desde el 2o caracter
+    }
+    else if (substr($src,0,3) == "../"){
+      $src = $scheme . "://" . $host . "/" . $src; //toma todo el texto desde el 2o caracter
+    }
+    else if (substr($src,0,5) != "https" && substr($src,0,4) != "http"){
+      $src = $scheme . "://" . $host . "/" . $src;
+    }
+
+    return $src;
+}
+
 
 function followLinks($url) {
     // Se crea un nuevo objeto. Recordar como funcionan las variables
@@ -11,10 +34,22 @@ function followLinks($url) {
 
     foreach($linkList as $link) {
       $href = $link->getAttribute("href");
+
+      if(strpos($href, "#") !== false){
+        continue;
+        //Continue Corta ejecución y hace que siga el siguiente LINK en el arreglo
+      }
+      else if(substr($href,0,11) == "javascript:"){
+        //Se pueden ejecutar funciones de javascript con el link respectivo
+        continue;
+      }
+
+      $href= createLink($href,$url);
+
       echo $href . "<br>";
-      // code...
+
     }
 }
-$startUrl = "http://xvideos.com";
+$startUrl = "http://gentesdelcomun.com";
 followLinks($startUrl);
  ?>
